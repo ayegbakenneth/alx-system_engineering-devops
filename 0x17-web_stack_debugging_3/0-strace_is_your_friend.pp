585b490986bfccce4ec13b744b3bec166365b685
ayegbakenneth <ayegbakenneth@gmail.com>
2024-05-16 01:36:51 -0700

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,44 +1,6 @@
-# Install and enable Apache package
-package { 'apache2':
-  ensure => installed,
-}
+# This helps to fix the Apache server 500 interner error.
 
-# Ensure Apache service is running and enabled
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['apache2'],
-}
-
-# Modify Apache configuration
-file { '/etc/apache2/apache2.conf':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('your_module_name/apache2.conf.erb'),
-  require => Package['apache2'],
-  notify  => Service['apache2'],
-}
-
-# Enable necessary Apache modules
-apache::mod { 'rewrite':
-  ensure => present,
-  before => File['/etc/apache2/apache2.conf'],
-  require => Package['apache2'],
-  notify => Service['apache2'],
-}
-
-apache::mod { 'headers':
-  ensure => present,
-  before => File['/etc/apache2/apache2.conf'],
-  require => Package['apache2'],
-  notify => Service['apache2'],
-}
-
-# Restart Apache service if configuration changes
-exec { 'restart_apache2':
-  command     => '/usr/sbin/service apache2 restart',
-  refreshonly => true,
-  subscribe   => File['/etc/apache2/apache2.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
